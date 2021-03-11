1fafeb0f690e088256e0f9629f2228ca787367a6
Kshitij Nigam <nigamelastic@gmail.com>
2021-03-11 10:51:33 +0100

    Update compareText_IP.py

--- compareText_IP.py
+++ compareText_IP.py
@@ -8,6 +8,7 @@ outputDiff='output\\outputDifference.csv'
 
 
 
+
 file1Compare1 = open(compare1, 'r')
 compare1Lines = file1Compare1.readlines()
 compare1Lines = sorted(compare1Lines)
@@ -19,24 +20,24 @@ compare2Lines = sorted(compare2Lines)
 outputDiffFile= open(outputDiff, 'a')
 
 
-outputDiffFile.write('Matches from both the files are below:\n')
+outputDiffFile.write('\nMatches from both the files are below:\n')
 
 for line in compare2Lines:
     if line in compare1Lines:
         compare1Lines.remove(line)
         compare2Lines.remove(line)
-        outputDiffFile.write(line+"\n")
+        outputDiffFile.write(line)
         
 
 
-outputDiffFile.write('Below are non duplicate entries from'+compare1 +'\n')
+outputDiffFile.write('\nBelow are non duplicate entries from -->'+compare1 +'\n')
 for value1 in compare1Lines:
-    outputDiffFile.write(value1+"\n")
+    outputDiffFile.write(value1)
 
-outputDiffFile.write('Below are non duplicate entries from'+compare2 +'\n')
+outputDiffFile.write('\nBelow are non duplicate entries from -->'+compare2 +'\n')
 for value2 in compare2Lines:
-    outputDiffFile.write(value2+"\n")    
+    outputDiffFile.write(value2)    
        
         
-outputDiffFile.write('Below are non duplicate entries from'+compare2 +'\n')
+
 outputDiffFile.close()
