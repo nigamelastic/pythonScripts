07d04ba3db92030e4221bcb08812476fc585e4e5
Kshitij Nigam <nigamelastic@gmail.com>
2021-05-06 01:00:21 +0200

    Create EnergieGCSH.sh
    the shell scrip for invoking GCSH.py

---
+++ GCSHPythonScripts/EnergieGCSH.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# --------------CronTab-----------------------
+# 0 6 * * * ~/EnergieGCSH.sh
+
+#sudo apt install chromium -y
+#wget https://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_linux64.zip
+#unzip chromedriver_linux64.zip
+#sudo mv chromedriver /usr/bin/
+#chmod +x ~/EnergieGCSH.py
+#python3 ~/EnergieGCSH.py
+
