fbccc388245792aff1208e251f0026168c155c97
Kshitij Nigam <nigamelastic@gmail.com>
2021-09-09 11:10:41 +0200

    Update zipFile.py

--- FileOperations/zipFile.py
+++ FileOperations/zipFile.py
@@ -3,6 +3,7 @@ import zipfile
 def unzipAndMove(folderLocation):
     folder = os.listdir(folderLocation)
     csvFolder=folderLocation+r'\\csvFiles'
+    #making a directory
     os.mkdir(csvFolder)
 
     for zipFileValue in folder:
@@ -11,11 +12,17 @@ def unzipAndMove(folderLocation):
             zipinfos = zipdata.infolist()
             # print(zipinfos)
             for zipinfo in zipinfos:
+                #changing the Filename value in the archive
                 zipinfo.filename=zipFileValue+r'_'+zipinfo.filename
                 print(zipinfo.filename)
+                #extracting the file with specific folder location
                 zipdata.extract(zipinfo,csvFolder)
+                
+                
+                
     for fileValue in folder:
         if fileValue.endswith(".csv"):
-            print(fileValue)
+            
             print(folderLocation+fileValue)
+            #moving the file
             os.rename(folderLocation+r'\\'+fileValue,csvFolder+r'\\'+fileValue)
